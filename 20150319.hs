3097ad9ddf803233b438bda02a2794766367cc55
Fabrízio <fbp@cin.ufpe.br>
2015-03-19 10:16:20 -0300

    Update and rename doubleList.hs to 20150319.hs

---
+++ 20150319.hs
@@ -0,0 +1,34 @@
+double :: [Int] -> [Int]
+double list
+ | list == [] = []
+ | otherwise = ((head list)*2) : (double (tail list))
+ 
+ {--}
+ 
+member :: [Int] -> Int-> Bool
+member membros membro
+ | membros == [] = False
+ | head membros == membro = True
+ | otherwise = member (tail membros) membro
+ 
+ {--}
+ 
+num :: String -> Char -> Bool
+num s c
+ | s == [] = False
+ | (head s) == c = True
+ | otherwise = num (tail s) c
+
+digits :: String -> String
+digits s
+ | s == [] = []
+ | num ['0'..'9'] (head s) = (head s) : digits (tail s)
+ | otherwise = digits (tail s)
+ 
+{--}
+
+sumPairs :: [Int] -> [Int] -> [Int]
+sumPairs x y
+ | x == [] = y
+ | y == [] = x
+ | otherwise = (head x) + (head y) : sumPairs (tail x) (tail y) 
--- doubleList.hs
+++
@@ -1,4 +0,0 @@
-double :: [Int] -> [Int]
-double list
- | list == [] = []
- | otherwise = ((head list)*2) : (double (tail list))
